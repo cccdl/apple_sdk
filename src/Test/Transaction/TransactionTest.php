1b4e054c370fe34c9ca686f7f583f758061876f1
dlc <568078220@qq.com>
2024-09-19 10:37:57 +0800

    苹果内购

--- src/Test/Transaction/TransactionTest.php
+++ src/Test/Transaction/TransactionTest.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace cccdl\apple_sdk\Test\History;
+namespace cccdl\apple_sdk\Test\Transaction;
 
-use cccdl\apple_sdk\Core\History\History;
+use cccdl\apple_sdk\Core\Transaction\Transaction;
 use cccdl\apple_sdk\Exceptions\cccdlException;
 use cccdl\apple_sdk\Test\Config;
 use GuzzleHttp\Exception\GuzzleException;
@@ -10,20 +10,19 @@ use PHPUnit\Framework\TestCase;
 
 require '../../../vendor/autoload.php';
 
-class HistoryTest extends TestCase
+class TransactionTest extends TestCase
 {
     /**
      * @return void
      * @throws GuzzleException
      * @throws cccdlException
      */
-    public function testGetTransactionHistory()
+    public function testGetTransactionInfo()
     {
         $config = Config::getConfig();
-        $app = new History($config, true);
-        $res = $app->getTransactionHistory('aaaa', []);
-        $data = $app->decodeSignedTransactions($res['signedTransactions']);
-        $this->assertIsArray($data);
+        $app = new Transaction($config, true);
+        $res = $app->getTransactionInfo('transactionId');
+        $this->assertIsArray($res);
 //        $this->assertArrayHasKey('status', $res);
 //        $this->assertSame(0, $res['status']);
     }
